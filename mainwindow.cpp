e2f4f9ad6fba944e14660b3f0e51f36f1b903986
Anton <mr.alligator@yandex.ru>
2020-02-09 16:58:33 +0300

    project start

---
+++ mainwindow.cpp
@@ -0,0 +1,25 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_ButtonConnect_clicked()
+{
+    ui->listWidget->addItem(ui->IP_Edit->text());
+}
+
+void MainWindow::on_ButtonDisconnect_clicked()
+{
+    ui->listWidget->clear();
+}
